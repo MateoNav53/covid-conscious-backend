0db8571376dbb6fa6d16f114a4523f012441fac5
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-16 14:51:25 -0700

    created preliminary post and get routes

--- routes/log.js
+++ routes/log.js
@@ -1,8 +1,37 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+let Log = require('../models/log.model');
 
-router.get('/login', (req, res) => res.send('Login Page'));
-router.get('/register', (req, res) => res.send('Register Page'));
+router.route('/').get((req, res) => {
+    Log.find()
+        .then(covidLogs => res.json(covidLogs))
+        .catch(err => res.status(400).json(err));
+});
+
+router.route('/:id').get((req, res) => {
+    let id = req.params.id;
+    Log.findById(id)
+        .then(log => res.json(log))
+        .catch(err => res.status(400).json(err));
+})
+
+router.route('/add').post((req, res) => {
+    const logDate = Date.parse(req.body.date);
+    const location = req.body.location;
+    const duration = Number(req.body.duration);
+    const interactions = Number(req.body.interactions);
+
+    const newCovidLog = new Log({
+        logDate,
+        location,
+        duration,
+        interactions
+    })
+
+    newCovidLog.save()
+    .then(() => res.json('Covid log added'))
+    .catch((err) => res.status(400).json(err));
+});
+
 
 module.exports = router;
 
