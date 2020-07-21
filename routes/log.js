9aec25f01c1002b53d928955bdc5bcf0547e099b
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-20 17:10:29 -0700

    changed error handling

--- routes/log.js
+++ routes/log.js
@@ -4,20 +4,20 @@ let Log = require('../models/log.model');
 router.route('/').get((req, res) => {
     Log.find()
         .then(covidLogs => res.json(covidLogs))
-        .catch(err => res.status(400).json(err));
+        .catch(err => res.json(err));
 });
 
 router.route('/:id').get((req, res) => {
     let id = req.params.id;
     Log.findById(id)
         .then(log => res.json(log))
-        .catch(err => res.status(400).json(err));
+        .catch(err => res.json(err));
 })
 
 router.route('/:id').delete((req, res) => {
     Log.findByIdAndDelete(req.params.id)
         .then(() => res.json('Covid log deleted'))
-        .catch((err) => res.status(400).json(err))
+        .catch((err) => res.json(err))
 })
 
 router.route('/add').post((req, res) => {
