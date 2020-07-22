0a43b285bb99591331b7aa4a30b89ac4633d690e
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-22 14:46:22 -0700

    built login and logout routes

--- routes/user.js
+++ routes/user.js
@@ -6,13 +6,23 @@ const JWT = require('jsonwebtoken');
 const User = require('../models/user.model');
 const Log = require('../models/log.model');
 
+const signToken = userID => {
+    // JWT.sign returns the actual jwt token
+    return JWT.sign({
+        iss: "MateoNav",
+        sub: userID
+    }, "MateoNav", {expiresIn: "1h"});
+}
+
 router.post('/register',(req,res) => {
     const{ username, fullname, email, password} = req.body;
+    // searches for the username
     User.findOne({username}, (err, user) => {
         if(err)
             res.status(err).json({message: {msgBody: "Error has occurred", msgError: true}})
         if(user)
             res.status(400).json({message: {msgBody: "Username is already taken", msgError: true}})
+        // if no error and if username entered isn't a duplicate, create this new username
         else {
             const newUser = new User({username, fullname, email, password});
             newUser.save(err => {
@@ -26,5 +36,18 @@ router.post('/register',(req,res) => {
 
     })
 })
+router.post('/login', passport.authenticate('local', {session: false}), (req, res) => {
+    if(req.isAuthenticated()){
+        const {_id, username} = req.user;
+        const token = signToken(_id);
+        res.cookie('access_token', token, {httpOnly: true, sameSite: true});
+        res.status(200).json({isAuthenticated: true, user: {username}});
+    }
+});
+
+router.get('/logout', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.clearCookie('access_token');
+    res.json({user: {username: ""}, success: true});
+});
 
 module.exports = router;
