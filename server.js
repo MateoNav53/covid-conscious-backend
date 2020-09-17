3f0619e391c6ecd6c297d6d2d216feef5f77055e
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-09-17 13:21:53 -0700

    added settings for heroku deployment

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 7000;
 
-const User = require('./models/user.model');
-
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/user', require('./routes/user'))
+app.get('/', (req, res) => { res.send('Hello from Express!')})
 
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
