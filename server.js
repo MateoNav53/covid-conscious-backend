198fa75c7c7855690c452dd2a5a856d1129456d7
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-10-01 20:25:15 -0700

    cors testing

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 7000;
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://mateonav53.github.io'],
+    optionsSuccessStatus: 200
+  }
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
