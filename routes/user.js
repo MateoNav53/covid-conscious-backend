78e9c3d5593a4a2cf27ca3d7df96eef946ccc524
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-10-05 15:51:51 -0700

    added domain to clearcookie, testing changes

--- routes/user.js
+++ routes/user.js
@@ -51,7 +51,7 @@ router.post('/login', passport.authenticate('local', {session: false}), (req, re
 });
 
 router.get('/logout', passport.authenticate('jwt', {session: false}), (req, res) => {
-    res.clearCookie('jwt');
+    res.clearCookie('jwt', {domain: 'covid-conscious.herokuapp.com'});
     res.json({user: {username: ""}, success: true});
 });
 
